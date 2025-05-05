c8eb27be73fc70eb84d17a226dbb87193adec83b
João Contarim <joaocontar@hotmail.com>
2025-05-05 18:46:36 -0300

    feat: mantém o tamanho escolhido após a geração

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app.config['ENV'] = 'production'
 # Função para gerar a senha
 def gerar_senha(tamanho=12, usar_maiusculas=True, usar_numeros=True, usar_simbolos=True):
     caracteres = string.ascii_lowercase
-    
+
     if usar_maiusculas:
         caracteres += string.ascii_uppercase
     if usar_numeros:
         caracteres += string.digits
 
     simbolos_permitidos = "!@#$%&*-+?/\\"
-    
+
     if usar_simbolos:
         caracteres += simbolos_permitidos
 
@@ -27,15 +27,27 @@ def gerar_senha(tamanho=12, usar_maiusculas=True, usar_numeros=True, usar_simbol
 @app.route('/', methods=['GET', 'POST'])
 def index():
     senha = None
+    tamanho = 12
+    usar_maiusculas = True
+    usar_numeros = True
+    usar_simbolos = True
+
     if request.method == 'POST':
         tamanho = int(request.form.get('tamanho', 12))
         usar_maiusculas = 'maiusculas' in request.form
         usar_numeros = 'numeros' in request.form
         usar_simbolos = 'simbolos' in request.form
-        
+
         senha = gerar_senha(tamanho, usar_maiusculas, usar_numeros, usar_simbolos)
 
-    return render_template('index.html', senha=senha)
+    return render_template(
+        'index.html',
+        senha=senha,
+        tamanho=tamanho,
+        usar_maiusculas=usar_maiusculas,
+        usar_numeros=usar_numeros,
+        usar_simbolos=usar_simbolos
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
