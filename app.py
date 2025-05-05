10af7cdb709031d2b3aecf448782917a397eff5b
João Contarim <150803938+Contarim@users.noreply.github.com>
2025-05-05 15:03:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import random
 import string
 import os
@@ -10,39 +10,32 @@ app.config['ENV'] = 'production'
 
 # Função para gerar a senha
 def gerar_senha(tamanho=12, usar_maiusculas=True, usar_numeros=True, usar_simbolos=True):
-    # Caracteres básicos
     caracteres = string.ascii_lowercase
     
-    # Adicionar letras maiúsculas, números e símbolos com base nas opções
     if usar_maiusculas:
         caracteres += string.ascii_uppercase
     if usar_numeros:
         caracteres += string.digits
 
-    # Definir a lista de símbolos personalizados
-    simbolos_permitidos = "!@#$%&*-+?\/"
+    simbolos_permitidos = "!@#$%&*-+?/\\"
     
     if usar_simbolos:
-        caracteres += simbolos_permitidos  # Apenas os símbolos definidos na variável
+        caracteres += simbolos_permitidos
 
-    # Gerar a senha
     return ''.join(random.choice(caracteres) for _ in range(tamanho))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     senha = None
     if request.method == 'POST':
-        # Obter os valores do formulário
         tamanho = int(request.form.get('tamanho', 12))
         usar_maiusculas = 'maiusculas' in request.form
         usar_numeros = 'numeros' in request.form
         usar_simbolos = 'simbolos' in request.form
         
-        # Gerar a senha com as opções fornecidas
         senha = gerar_senha(tamanho, usar_maiusculas, usar_numeros, usar_simbolos)
 
     return render_template('index.html', senha=senha)
 
 if __name__ == '__main__':
-    # Certifique-se de que o Flask escute na porta correta no Render
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
